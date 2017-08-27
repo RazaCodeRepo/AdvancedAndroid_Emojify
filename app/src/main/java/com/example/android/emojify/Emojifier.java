ddf52c9257610057f3c40b5818fa59252e2aa619
RazaCodeRepo <raza.code.repo@gmail.com>
2017-08-27 22:18:23 +0500

    Detect faces not working

---
+++ app/src/main/java/com/example/android/emojify/Emojifier.java
@@ -0,0 +1,37 @@
+package com.example.android.emojify;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.Log;
+import android.util.SparseArray;
+import android.widget.Toast;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.face.Face;
+import com.google.android.gms.vision.face.FaceDetector;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * Created by Kontrol on 8/27/2017.
+ */
+
+public class Emojifier {
+
+    public static final String TAG = "Emojifier";
+
+    public static void detectFaces(Context context, Bitmap bitmap){
+
+        FaceDetector faceDetector = new FaceDetector.Builder(context).setTrackingEnabled(false).build();
+
+        Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+        SparseArray<Face> faces = faceDetector.detect(frame);
+
+        Log.v(TAG, "No of faces detected: " + faces.size());
+
+        if(faces.size() == 0){
+            Toast.makeText(context, "No Faces Detected", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+}
